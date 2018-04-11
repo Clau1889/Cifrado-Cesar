c80e1f4f8990e4af66e650fddd7703505ff96cfe
Clau1889 <clau.blanco1889@gmail.com>
2018-04-11 12:05:35 -0500

    Se modifican funciones y se le da formato de buenas praticas

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 do {
+
   do{
+
     var msj= prompt('Por favor, ingrese una palabra o frase a descifrar.');
+
       if(msj=== ''){//en esta condicion se validara que el usuario no ingrese un campo vacio.
           alert('Campo vacio. Recuerda ingresar una palabra o frase.');
           break;
+
       }else{//en esta condicion se iterara a cada elemento del mensaje para descartar caracteres diferentes a las letras de acuerdo con ayuda de  codigo Ascii.
         for (var i=0; i<msj.length; i++){
           var ascii=msj.charCodeAt(i);
@@ -13,43 +17,42 @@ do {
             break;
         }//cierre for
       }//cierre else
+
     }while((ascii<65 || ascii>90) && (ascii<97 || ascii>122));//mientras no se ingresen letras, pedira ingrasar una palabra.
+    
 }while(msj==='');//mientras no se ingrese nada, pedira ingresar una palabra.
 
 
 function cipher (message){//en esta funcion se ingresara al msj del usuario para pasarlo a un array de numeros de acuero al codigo Ascii
   var ascii;
   var arrayAscii= [];
-  var auxAscii;
-  var resultStr='';
-  var finalStr='';
+
 
     for(var j= 0; j<message.length; j++){//en itera sobre cada elemento del mensaje que contiene letras para posteriormente convertirlo a Ascii y obtener un array de numeros
       ascii=message.charCodeAt(j);
         arrayAscii.push(ascii);
         //console.log(arrayAscii);
     }//cierre for en j
-
-      for (var k=0; k<arrayAscii.length; k++){
-         auxAscii=((arrayAscii[k]) - 65 + 33) %26 +65;
-          //console.log(auxAscii);
-            resultStr=String.fromCharCode(auxAscii);
-             //console.log(resultStr);
-               finalStr += resultStr;
-                //console.log(finalStr);
-          }//cierre for in k
-          alert('Tu codigo cifrado es: ' + finalStr);
-    return finalStr;
+    return arrayAscii;
 
 }//cierre fuction
-cipher(msj);
-/*
-function descipher (msjCipher){
+function descipher (msjAscii){
+  var ascii;
+  var resultStr='';
+  var finalStr='';
 
+  for (var k=0; k<msjAscii.length; k++){
 
+    ascii=(msjAscii[k]- 65 +33) %26 +65;
 
+      resultStr=String.fromCharCode(ascii);
 
-  }//cierre for
+        finalStr+= resultStr;
+    }//cierre for
+
+    alert('Tu codigo cifrado es: ' + finalStr);
+
+    return finalStr;
 }//function
+
 descipher(cipher(msj));
-*/
